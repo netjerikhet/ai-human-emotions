527e6361b55efbc0b61c7574e9f1f73aa0e13690
kaushikdayalan <kaushikruth1@gmail.com>
2022-07-05 20:34:14 +0200

    bug fixes in scripts

--- model/util.py
+++ model/util.py
@@ -25,9 +25,9 @@ def kp2gaussian(kp, spatial_size, kp_variance):
     Transforming a keypoint into gaussian like representation
     """
     mean = kp['value']
-    coordinate_grid = make_coordinate_grid(spatial_size, mean.type)
+    coordinate_grid = make_coordinate_grid(spatial_size, mean.type())
     number_of_leading_dimensions = len(mean.shape) - 1
-    shape = (1,) * number_of_leading_dimensions + coordinate_grid.final_shape
+    shape = (1,) * number_of_leading_dimensions + coordinate_grid.shape
     coordinate_grid = coordinate_grid.view(*shape)
     repeats = mean.shape[:number_of_leading_dimensions] + (1, 1, 1)
     coordinate_grid = coordinate_grid.repeat(*repeats)
